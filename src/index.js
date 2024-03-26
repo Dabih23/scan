da63de795225be20d018f2a77cff408992a5824c
Dabih23 <Roman-Turchenko2002@yandex.ru>
2024-03-26 08:33:37 +0300

    v3

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ export const Context = createContext({
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React>
+  <React.StrictMode>
     <Context.Provider value={{
       store
     }}>
@@ -21,7 +21,7 @@ root.render(
         <App />
       </HashRouter>
     </Context.Provider>
-  </React>
+  </React.StrictMode>
 ); 
 
 
